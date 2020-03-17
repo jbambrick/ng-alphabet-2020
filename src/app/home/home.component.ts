6cfea4cffd02c287c2b665f1991dde8fcc348077
unknown <aplahn85@gmail.com>
2020-03-17 15:57:56 -0700

    initial commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  title = 'ng-alphabet-2020';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+}
