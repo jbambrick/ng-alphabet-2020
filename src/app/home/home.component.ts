e7e97401eef0dda70abb5ff849789285b03fc6ba
unknown <aplahn85@gmail.com>
2020-03-17 16:18:40 -0700

    Add title and logo to home component.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  title = 'ng-alphabet-2020';
+  title = 'Tŝilhqot’in Alphabet';
 
   constructor() { }
 
