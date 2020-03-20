717158b61d25a6bbc92d9dcd7de73816a8042303
unknown <aplahn85@gmail.com>
2020-03-20 14:35:30 -0700

    Implement menu with hard coded data.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -8,6 +8,28 @@ import { ElementRef } from '@angular/core';
   styleUrls: ['./menu.component.css']
 })
 export class MenuComponent implements OnInit {
+  tiles: string[] = ['1','2','3'];
+  private numberOfLettersInAlphabet: number = 53;
+  private numberOfMenuRows: number = 3;
+  private numberOfMenuColumns: number = 3;
+  private numberOfMenuItems: number = this.numberOfMenuColumns*this.numberOfMenuRows; // calculate for consistency
+
+  menuRows = 
+  [[ 
+    {id:"1"},
+    {id:"7"},
+    {id:"13"}
+  ],
+  [ 
+    {id:"19"},
+    {id:"25"},
+    {id:"31"}
+  ],
+  [ 
+    {id:"37"},
+    {id:"43"},
+    {id:"49"}
+  ]];
 
   constructor() { }
 
