8a0b196b29c5a556e6bf15ee9ac93b998badf366
unknown <aplahn85@gmail.com>
2020-03-25 12:07:48 -0700

    Implement audio service. Audio now functional.

--- src/app/audio.service.ts
+++ src/app/audio.service.ts
@@ -4,10 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AudioService {
-
+  private audioFile = new Audio();
   constructor() { }
 
   playAudio(audioFilePath: string){
     console.log(`Playing audio file: ${audioFilePath}`);
+    this.audioFile.src = audioFilePath;
+    this.audioFile.play();
   }
 }
